f3ed6d93c333aaf64b13284a8ac4347de6c9b836
Joao Souza <joaohsistemas@gmail.com>
2018-04-01 19:13:27 -0300

    Reconfiguração do webpack para utilizar index.js ao inves de main.js; Instalação e implementação do react-router.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Feed from './containers/feed.js';
+import Login from './components/login.js';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
-		<App />
+		<BrowserRouter>
+	  		<div>
+	  			<Switch>
+	  				<Route path="/feed" component={Feed} />	  				
+	  				<Route path="/" component={Login} />
+	  			</Switch>
+	  		</div>
+  		</BrowserRouter>
 	</Provider>
   	, document.querySelector('.container')
 );
