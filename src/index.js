531c7167d5a49462b12e13226d37cbe32a66ae97
Joao Souza <joaohsistemas@gmail.com>
2018-04-02 21:12:52 -0300

    Adicionado redux-form ao projeto e inicio da autenticação por JWT

--- src/index.js
+++ src/index.js
@@ -10,6 +10,17 @@ import Feed from './components/feed.js';
 import Login from './components/login.js';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+//busca o token no localStorage do browser
+//const token = localStorage.getItem('token');
+
+//Verifica se o token = true, se sim, renderiza outras paginas
+/*
+if (token) {
+	store.dispatch({ type: AUTENTICADO });
+}
+*/
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
